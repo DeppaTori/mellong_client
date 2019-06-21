9672b3b565016894e358fb26136a6a87caf166a8
Tommy Toban <tommy.deppatori@gmail.com>
2019-06-21 19:39:07 +0700

    add route

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import Hello from './components/StatefulHello';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from './components/Home';
 import Contact from './components/Contact'
 
 const routing = (
     <Router>
       <div>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/contact" component={Contact} />
       </div>
     </Router>
